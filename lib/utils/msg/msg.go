88664622f8f165646ef89c34a48211efae676c97
Kijima Daigo <norimaking777@gmail.com>
2023-11-26 14:32:18 +0900

    feat: メソッド追加した

--- lib/utils/msg/msg.go
+++ lib/utils/msg/msg.go
@@ -12,25 +12,67 @@ package msg
 
 // 改ページは押したときに現在の表示文字をフラッシュして、上から表示にすること。
 
-type MsgBuilder struct {
+type Msg struct {
 	pages []page
-	pos   int
+	pos   int // pageのpos
+	buf   string
+}
+
+func (m *Msg) Buf() string {
+	str, _ := m.submit()
+	// submit statusがlineのとき、\nを入れる
+	//                pageのとき、bufを消す
+	//                msgのとき、終了ステータスを返す
+	// 送りオプションがついてるときだけ、次に進めるようにする
+	m.buf = m.buf + str
+	return m.buf
+}
+
+func (m *Msg) submit() (string, bool) {
+	if m.pos > len(m.pages)-1 {
+		return "", false
+	}
+
+	str, ok := m.pages[m.pos].submit()
+	if !ok {
+		m.pos++
+		str, ok := m.submit()
+		return str, ok
+	}
+	return str, true
 }
 
 type page struct {
 	lines []line
-	pos   int
+	pos   int // linesのpos
+}
+
+func (p *page) submit() (string, bool) {
+	if p.pos > len(p.lines)-1 {
+		return "", false
+	}
+	str, ok := p.lines[p.pos].submit()
+	if !ok {
+		p.pos++
+		str, ok := p.submit()
+		return str, ok
+	}
+	return str, true
 }
 
 // lineごとにクリック待ちが発生する。改行は入るかもしれない
 // 表示はstr1つ1つ
 type line struct {
 	str string
-	pos int
+	pos int // strのpos
 }
 
-type chars []string
-
-func New(raw string) *MsgBuilder {
-	return &MsgBuilder{}
+func (l *line) submit() (string, bool) {
+	// fmt.Println(l.pos, len(l.str)-1)
+	if l.pos > len(l.str)-1 {
+		return "", false
+	}
+	str := l.str[l.pos]
+	l.pos++
+	return string(str), true
 }
--- lib/utils/msg/msg_test.go
+++ lib/utils/msg/msg_test.go
@@ -7,24 +7,205 @@ import (
 )
 
 func TestNew(t *testing.T) {
-	str := `こんにちは。\n
-今日は晴れです。[n]
-ところで。[l]
-どうなりました?`
+	// 	str := `こんにちは。\n
+	// 今日は晴れです。[n]
+	// ところで。[l]
+	// どうなりました?`
 
-	page1 := page{lines: []line{
-		line{str: "こんにちは。\n今日は晴れです"},
-	}}
-	page2 := page{lines: []line{
-		line{str: "ところで"},
-		line{str: "どうなりました?"},
-	}}
-	expect := MsgBuilder{
-		pages: []page{
-			page1,
-			page2,
+	//	page1 := page{lines: []line{
+	//		line{str: "こんにちは。\n今日は晴れです"},
+	//	}}
+	//	page2 := page{lines: []line{
+	//		line{str: "ところで"},
+	//		line{str: "どうなりました?"},
+	//	}}
+	//	expect := MsgBuilder{
+	//		pages: []page{
+	//			page1,
+	//			page2,
+	//		},
+	//	}
+	//
+	// assert.Equal(t, expect, NewLexer(str))
+}
+
+func TestLineSubmit(t *testing.T) {
+	l := line{
+		str: "abc",
+	}
+
+	tests := []struct {
+		expectValue string
+		expectCont  bool
+	}{
+		{
+			expectValue: "a",
+			expectCont:  true,
+		},
+		{
+			expectValue: "b",
+			expectCont:  true,
+		},
+		{
+			expectValue: "c",
+			expectCont:  true,
+		},
+		{
+			expectValue: "",
+			expectCont:  false,
 		},
 	}
 
-	assert.Equal(t, expect, New(str))
+	for _, tt := range tests {
+		str, cont := l.submit()
+		assert.Equal(t, tt.expectValue, str)
+		assert.Equal(t, tt.expectCont, cont)
+	}
+}
+
+func TestPageSubmit(t *testing.T) {
+	p := page{
+		lines: []line{
+			{str: "ab"},
+			{str: "cd"},
+		},
+	}
+
+	tests := []struct {
+		expectValue string
+		expectCont  bool
+	}{
+		{
+			expectValue: "a",
+			expectCont:  true,
+		},
+		{
+			expectValue: "b",
+			expectCont:  true,
+		},
+		{
+			expectValue: "c",
+			expectCont:  true,
+		},
+		{
+			expectValue: "d",
+			expectCont:  true,
+		},
+		{
+			expectValue: "",
+			expectCont:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		str, cont := p.submit()
+		assert.Equal(t, tt.expectValue, str)
+		assert.Equal(t, tt.expectCont, cont)
+	}
+}
+
+func TestMsgSubmit(t *testing.T) {
+	m := Msg{
+		pages: []page{
+			{
+				lines: []line{
+					{str: "ab"},
+					{str: "cd"},
+				},
+			},
+			{
+				lines: []line{
+					{str: "ef"},
+					{str: "gh"},
+				},
+			},
+		},
+	}
+
+	tests := []struct {
+		expectValue string
+		expectCont  bool
+	}{
+		{
+			expectValue: "a",
+			expectCont:  true,
+		},
+		{
+			expectValue: "b",
+			expectCont:  true,
+		},
+		{
+			expectValue: "c",
+			expectCont:  true,
+		},
+		{
+			expectValue: "d",
+			expectCont:  true,
+		},
+		{
+			expectValue: "e",
+			expectCont:  true,
+		},
+		{
+			expectValue: "f",
+			expectCont:  true,
+		},
+		{
+			expectValue: "g",
+			expectCont:  true,
+		},
+		{
+			expectValue: "h",
+			expectCont:  true,
+		},
+		{
+			expectValue: "",
+			expectCont:  false,
+		},
+	}
+
+	for _, tt := range tests {
+		str, cont := m.submit()
+		assert.Equal(t, tt.expectValue, str)
+		assert.Equal(t, tt.expectCont, cont)
+	}
+}
+
+func TestMsgbuf(t *testing.T) {
+	m := Msg{
+		pages: []page{
+			{
+				lines: []line{
+					{str: "ab"},
+					{str: "cd"},
+				},
+			},
+			{
+				lines: []line{
+					{str: "ef"},
+					{str: "gh"},
+				},
+			},
+		},
+	}
+
+	str := m.Buf()
+	assert.Equal(t, "a", str)
+
+	str = m.Buf()
+	assert.Equal(t, "ab", str)
+
+	// フラグなしだと先に進まない
+	str = m.Buf()
+	assert.Equal(t, "ab", str)
+
+	str = m.Buf()
+	assert.Equal(t, "abc", str)
+
+	str = m.Buf()
+	assert.Equal(t, "abcd", str)
+
+	// フラグなしだと先に進まない
+	str = m.Buf()
+	assert.Equal(t, "abcd", str)
 }
