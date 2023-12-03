91a743df91d87c3b810556fe3a5aa104bfa59ddf
Kijima Daigo <norimaking777@gmail.com>
2023-12-04 01:11:33 +0900

    feat: 拠点の横メニューを追加した

--- lib/states/home_menu.go
+++ lib/states/home_menu.go
@@ -2,13 +2,19 @@
 package states
 
 import (
+	"fmt"
+
+	ec "github.com/kijimaD/sokotwo/lib/engine/components"
 	"github.com/kijimaD/sokotwo/lib/engine/loader"
 	"github.com/kijimaD/sokotwo/lib/engine/states"
 	w "github.com/kijimaD/sokotwo/lib/engine/world"
 	"github.com/kijimaD/sokotwo/lib/resources"
+	ecs "github.com/x-hgg-x/goecs/v2"
 )
 
-type HomeMenuState struct{}
+type HomeMenuState struct {
+	selection int
+}
 
 // State interface ================
 
@@ -26,5 +32,58 @@ func (st *HomeMenuState) OnStop(world w.World) {
 }
 
 func (st *HomeMenuState) Update(world w.World) states.Transition {
-	return states.Transition{}
+	world.Manager.Join(world.Components.Engine.Text, world.Components.Engine.UITransform).Visit(ecs.Visit(func(entity ecs.Entity) {
+		text := world.Components.Engine.Text.Get(entity).(*ec.Text)
+		if text.ID == "description" {
+			switch st.selection {
+			case 0:
+				text.Text = "遺跡に出発する"
+			case 1:
+				text.Text = "装備品やアイテムを購入する"
+			case 2:
+				text.Text = "装備変更/アイテムを使う"
+			case 3:
+				text.Text = "仲間を入れ替える"
+			case 4:
+				text.Text = "設定を変更する"
+			}
+		}
+	}))
+
+	return updateMenu(st, world)
+}
+
+// Menu Interface ================
+
+func (st *HomeMenuState) getSelection() int {
+	return st.selection
+}
+
+func (st *HomeMenuState) setSelection(selection int) {
+	st.selection = selection
+}
+
+func (st *HomeMenuState) confirmSelection(world w.World) states.Transition {
+	switch st.selection {
+	case 0:
+		return states.Transition{Type: states.TransSwitch, NewStates: []states.State{&FieldState{}}}
+	case 1:
+		return states.Transition{Type: states.TransSwitch, NewStates: []states.State{&FieldState{}}}
+	case 2:
+		return states.Transition{Type: states.TransSwitch, NewStates: []states.State{&FieldState{}}}
+	case 3:
+		return states.Transition{Type: states.TransSwitch, NewStates: []states.State{&FieldState{}}}
+	case 4:
+		return states.Transition{Type: states.TransSwitch, NewStates: []states.State{&FieldState{}}}
+
+	}
+	panic(fmt.Errorf("unknown selection: %d", st.selection))
+}
+
+func (st *HomeMenuState) getMenuIDs() []string {
+	return []string{"dungeon", "buy", "equip", "party", "system"}
+}
+
+func (st *HomeMenuState) getCursorMenuIDs() []string {
+	return []string{"cursor_dungeon", "cursor_buy", "cursor_equip", "cursor_party", "cursor_system"}
 }
