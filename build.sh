66ac4de44a42f594727d0893efcc2e97fa791acd
Kijima Daigo <norimaking777@gmail.com>
2023-12-03 15:33:33 +0900

    feat: add wasm

--- build.sh
+++ build.sh
@@ -34,10 +34,14 @@ start() {
 
     # cmd "go build -buildvcs=false -o ${SOURCE}_darwin_amd64 ." darwin amd64 1
     # cmd "go build -o ${SOURCE}_darwin_arm64 ." darwin arm64 1
+
     cmd "go build -buildvcs=false -o ${SOURCE}_linux_amd64 ." linux amd64 1
     # cmd "go build -buildvcs=false -o ${SOURCE}_linux_arm64 ." linux arm64 1
+
     cmd "go build -buildvcs=false -o ${SOURCE}_windows_amd64.exe ." windows amd64 0
     # cmd "go build -buildvcs=false -o ${SOURCE}_windows_arm64.exe ." windows arm64 0
+
+    cmd "go build -buildvcs=false -o ${SOURCE}_js_wasm.wasm ." js wasm 0
 }
 
 start
