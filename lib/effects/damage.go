61d66d5bc32420048f8606bd5c8a88b2af555c56
Kijima Daigo <norimaking777@gmail.com>
2023-12-24 01:32:09 +0900

    fix: wasmが表示されてない

--- lib/effects/damage.go
+++ lib/effects/damage.go
@@ -3,6 +3,7 @@ package effects
 import (
 	gc "github.com/kijimaD/sokotwo/lib/components"
 	w "github.com/kijimaD/sokotwo/lib/engine/world"
+	"github.com/kijimaD/sokotwo/lib/utils/mathutil"
 	ecs "github.com/x-hgg-x/goecs/v2"
 )
 
@@ -11,7 +12,7 @@ func InflictDamage(world w.World, damage EffectSpawner, target ecs.Entity) {
 	pools := gameComponents.Pools.Get(target).(*gc.Pools)
 	v, ok := damage.EffectType.(Damage)
 	if ok {
-		pools.HP.Current = max(0, pools.HP.Current-v.Amount)
+		pools.HP.Current = mathutil.Max(0, pools.HP.Current-v.Amount)
 	}
 }
 
@@ -20,6 +21,6 @@ func HealDamage(world w.World, healing EffectSpawner, target ecs.Entity) {
 	pools := gameComponents.Pools.Get(target).(*gc.Pools)
 	v, ok := healing.EffectType.(Healing)
 	if ok {
-		pools.HP.Current = min(pools.HP.Max, pools.HP.Current+v.Amount)
+		pools.HP.Current = mathutil.Min(pools.HP.Max, pools.HP.Current+v.Amount)
 	}
 }
