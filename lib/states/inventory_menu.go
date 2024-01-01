0a9158e7080820fa2ef0ac2a54eeed67b3debac2
Kijima Daigo <norimaking777@gmail.com>
2024-01-01 20:20:52 +0900

    refactor: ヘルパー関数に移動した

--- lib/states/inventory_menu.go
+++ lib/states/inventory_menu.go
@@ -3,7 +3,6 @@ package states
 import (
 	"image"
 	"image/color"
-	"math"
 
 	"github.com/ebitenui/ebitenui"
 	e_image "github.com/ebitenui/ebitenui/image"
@@ -276,49 +275,9 @@ func (st *InventoryMenuState) initUI(world w.World) *ebitenui.UI {
 		content.AddChild(button)
 	}
 
-	scrollContainer := widget.NewScrollContainer(
-		widget.ScrollContainerOpts.Content(content),
-		widget.ScrollContainerOpts.StretchContentWidth(),
-		widget.ScrollContainerOpts.Image(&widget.ScrollContainerImage{
-			Idle: e_image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-			Mask: e_image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-		}),
-	)
-	rootContainer.AddChild(scrollContainer)
-
-	pageSizeFunc := func() int {
-		return int(math.Round(float64(scrollContainer.ContentRect().Dy()) / float64(content.GetWidget().Rect.Dy()) * 1000))
-	}
-	trackPadding := widget.Insets{4, 20, 20, 4}
-	vSlider := widget.NewSlider(
-		widget.SliderOpts.Direction(widget.DirectionVertical),
-		widget.SliderOpts.MinMax(0, 1000),
-		widget.SliderOpts.PageSizeFunc(pageSizeFunc),
-		widget.SliderOpts.ChangedHandler(func(args *widget.SliderChangedEventArgs) {
-			scrollContainer.ScrollTop = float64(args.Slider.Current) / 1000
-		}),
-		widget.SliderOpts.Images(
-			&widget.SliderTrackImage{
-				Idle:  e_image.NewNineSliceColor(color.NRGBA{100, 100, 100, 0}),
-				Hover: e_image.NewNineSliceColor(color.NRGBA{100, 100, 100, 0}),
-			},
-			&widget.ButtonImage{
-				Idle:    e_image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
-				Hover:   e_image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
-				Pressed: e_image.NewNineSliceColor(color.NRGBA{255, 100, 100, 255}),
-			},
-		),
-		widget.SliderOpts.TrackPadding(trackPadding),
-	)
-	scrollContainer.GetWidget().ScrolledEvent.AddHandler(func(args interface{}) {
-		a := args.(*widget.WidgetScrolledEventArgs)
-		p := pageSizeFunc() / 3
-		if p < 1 {
-			p = 1
-		}
-		vSlider.Current -= int(math.Round(a.Y * float64(p)))
-	})
-	rootContainer.AddChild(vSlider)
+	sc, v := eui.NewScrollContainer(content)
+	rootContainer.AddChild(sc)
+	rootContainer.AddChild(v)
 
 	itemSpec := widget.NewText(
 		widget.TextOpts.Text("性能", face, color.White),
