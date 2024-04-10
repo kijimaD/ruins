57df3357d273471375e34df70ae26815e2f14e8e
Kijima Daigo <norimaking777@gmail.com>
2024-04-11 01:08:12 +0900

    feat: カメラの視界を設定する

--- lib/systems/grid_transform.go
+++ lib/systems/grid_transform.go
@@ -1,6 +1,10 @@
 package systems
 
 import (
+	"math"
+
+	mymath "github.com/kijimaD/ruins/lib/engine/math"
+
 	gc "github.com/kijimaD/ruins/lib/components"
 
 	ec "github.com/kijimaD/ruins/lib/engine/components"
@@ -40,7 +44,6 @@ func GridTransformSystem(world w.World) {
 		world.Components.Engine.Transform,
 	).Visit(ecs.Visit(func(entity ecs.Entity) {
 		gridElement := gameComponents.GridElement.Get(entity).(*gc.GridElement)
-
 		elementSpriteRender := world.Components.Engine.SpriteRender.Get(entity).(*ec.SpriteRender)
 		elementTranslation := &world.Components.Engine.Transform.Get(entity).(*ec.Transform).Translation
 
@@ -49,5 +52,15 @@ func GridTransformSystem(world w.World) {
 
 		elementTranslation.X = float64(gridElement.Col*elementSprite.Width) + float64(elementSprite.Width)/2 + transformOffsetX - float64(playerY*tileSize)
 		elementTranslation.Y = screenHeight - float64(gridElement.Line*elementSprite.Height) - float64(elementSprite.Height)/2 + transformOffsetY + float64(playerX*tileSize)
+
+		// 視界
+		xAbs := mymath.Abs(gridElement.Line - playerX)
+		yAbs := mymath.Abs(gridElement.Col - playerY)
+		if math.Sqrt(float64(math.Pow(float64(xAbs), 2))+float64(math.Pow(float64(yAbs), 2))) > 6 {
+			// 描画範囲外に移動させて非表示にする...
+			// TODO: 遠さによって透明度を変えつつ、黒で塗りつぶしたい
+			elementTranslation.X = -100
+			elementTranslation.Y = -100
+		}
 	}))
 }
