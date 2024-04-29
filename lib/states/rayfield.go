9cec750751af07f4ac8c6ed936e4692b99315682
Kijima Daigo <norimaking777@gmail.com>
2024-04-29 15:17:59 +0900

    feat: stateに移動する

--- lib/states/rayfield.go
+++ lib/states/rayfield.go
@@ -4,9 +4,12 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/kijimaD/ruins/lib/engine/states"
 	w "github.com/kijimaD/ruins/lib/engine/world"
+	"github.com/kijimaD/ruins/lib/raycast"
 )
 
-type RayFieldState struct{}
+type RayFieldState struct {
+	Game raycast.Game
+}
 
 func (st RayFieldState) String() string {
 	return "RayField"
@@ -19,6 +22,9 @@ func (st *RayFieldState) OnPause(world w.World) {}
 func (st *RayFieldState) OnResume(world w.World) {}
 
 func (st *RayFieldState) OnStart(world w.World) {
+	st.Game.Px = 100
+	st.Game.Py = 100
+	st.Game.Prepare()
 }
 
 func (st *RayFieldState) OnStop(world w.World) {
@@ -26,7 +32,11 @@ func (st *RayFieldState) OnStop(world w.World) {
 }
 
 func (st *RayFieldState) Update(world w.World) states.Transition {
+	st.Game.Update()
+
 	return states.Transition{}
 }
 
-func (st *RayFieldState) Draw(world w.World, screen *ebiten.Image) {}
+func (st *RayFieldState) Draw(world w.World, screen *ebiten.Image) {
+	st.Game.Draw(screen)
+}
