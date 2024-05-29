e186ec29bfab502c79ae5f5a93cdb03c428744df
Kijima Daigo <norimaking777@gmail.com>
2024-05-29 23:41:51 +0900

    feat: 1マスの通路を移動しやすくする

--- lib/mapbuilder/rect_room.go
+++ lib/mapbuilder/rect_room.go
@@ -15,8 +15,8 @@ func (b RectRoomBuilder) BuildRooms(buildData *BuilderMap) {
 	const maxRooms = 8
 	rooms := []Rect{}
 	for i := 0; i < maxRooms; i++ {
-		x := 0 + rand.Intn(16)
-		y := 0 + rand.Intn(16)
+		x := 1 + rand.Intn(16)
+		y := 1 + rand.Intn(16)
 		w := 2 + rand.Intn(2)
 		h := 2 + rand.Intn(2)
 		newRoom := Rect{x, y, x + w, y + h}
