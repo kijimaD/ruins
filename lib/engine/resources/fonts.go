eefcb6eb868708e7f3117097f5d22e9b982faddd
Kijima Daigo <norimaking777@gmail.com>
2024-10-14 01:51:16 +0900

    feat: ebitenuiからコピペしてきた
    https://github.com/ebitenui/ebitenui/blob/5729607498b38ae7cdbb01d7a299b26007779fee/_examples/demo/resources.go

---
+++ lib/engine/resources/fonts.go
@@ -0,0 +1,66 @@
+package resources
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2/text/v2"
+)
+
+const (
+	fontFaceRegular = "assets/fonts/NotoSans-Regular.ttf"
+	fontFaceBold    = "assets/fonts/NotoSans-Bold.ttf"
+)
+
+type fonts struct {
+	face         text.Face
+	titleFace    text.Face
+	bigTitleFace text.Face
+	toolTipFace  text.Face
+}
+
+func loadFonts() (*fonts, error) {
+	fontFace, err := loadFont(fontFaceRegular, 20)
+	if err != nil {
+		return nil, err
+	}
+
+	titleFontFace, err := loadFont(fontFaceBold, 24)
+	if err != nil {
+		return nil, err
+	}
+
+	bigTitleFontFace, err := loadFont(fontFaceBold, 28)
+	if err != nil {
+		return nil, err
+	}
+
+	toolTipFace, err := loadFont(fontFaceRegular, 15)
+	if err != nil {
+		return nil, err
+	}
+
+	return &fonts{
+		face:         fontFace,
+		titleFace:    titleFontFace,
+		bigTitleFace: bigTitleFontFace,
+		toolTipFace:  toolTipFace,
+	}, nil
+}
+
+func loadFont(path string, size float64) (text.Face, error) {
+	fontFile, err := embeddedAssets.Open(path)
+	if err != nil {
+		return nil, err
+	}
+
+	s, err := text.NewGoTextFaceSource(fontFile)
+	if err != nil {
+		log.Fatal(err)
+		return nil, err
+	}
+
+	return &text.GoTextFace{
+		Source: s,
+		Size:   size,
+	}, nil
+}
