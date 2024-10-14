7f9ced0089db8f66b8de63e3cf539afddd7a2abf
Kijima Daigo <norimaking777@gmail.com>
2024-10-14 14:58:54 +0900

    feat: progressbar

--- lib/views/member.go
+++ lib/views/member.go
@@ -2,9 +2,7 @@ package views
 
 import (
 	"fmt"
-	"image/color"
 
-	"github.com/ebitenui/ebitenui/image"
 	"github.com/ebitenui/ebitenui/widget"
 	gc "github.com/kijimaD/ruins/lib/components"
 	w "github.com/kijimaD/ruins/lib/engine/world"
@@ -24,32 +22,25 @@ func AddMemberBar(world w.World, targetContainer *widget.Container, entity ecs.E
 	pools := gameComponents.Pools.Get(entity).(*gc.Pools)
 	memberContainer.AddChild(eui.NewMenuText(fmt.Sprintf("%s %3d/%3d", consts.HPLabel, pools.HP.Current, pools.HP.Max), world))
 
+	res := world.Resources.UIResources
 	hProgressbar := widget.NewProgressBar(
 		widget.ProgressBarOpts.WidgetOpts(
-			// Set the minimum size for the progress bar.
-			// This is necessary if you wish to have the progress bar be larger than
-			// the provided track image. In this exampe since we are using NineSliceColor
-			// which is 1px x 1px we must set a minimum size.
 			widget.WidgetOpts.MinSize(140, 20),
+			widget.WidgetOpts.LayoutData(widget.RowLayoutData{
+				Position: widget.RowLayoutPositionCenter},
+			),
 		),
 		widget.ProgressBarOpts.Images(
-			// Set the track images (Idle, Hover, Disabled).
-			&widget.ProgressBarImage{
-				Idle:  image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-				Hover: image.NewNineSliceColor(color.NRGBA{100, 100, 100, 255}),
-			},
-			// Set the progress images (Idle, Hover, Disabled).
-			&widget.ProgressBarImage{
-				Idle:  image.NewNineSliceColor(color.NRGBA{0, 255, 0, 255}),
-				Hover: image.NewNineSliceColor(color.NRGBA{0, 0, 255, 255}),
-			},
+			res.ProgressBar.TrackImage,
+			res.ProgressBar.FillImage,
 		),
-		// min, max, current
-		widget.ProgressBarOpts.Values(0, pools.HP.Max, pools.HP.Current),
 		widget.ProgressBarOpts.TrackPadding(widget.Insets{
-			Top:    2,
-			Bottom: 2,
+			Top:    3,
+			Bottom: 3,
+			Left:   2,
+			Right:  2,
 		}),
+		widget.ProgressBarOpts.Values(0, pools.HP.Max, pools.HP.Current),
 	)
 	memberContainer.AddChild(hProgressbar)
 	memberContainer.AddChild(eui.NewMenuText(fmt.Sprintf("LV %d", pools.Level), world))
