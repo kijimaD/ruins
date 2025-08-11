7deaece1dc13c58d7b865210dcf2ef6134c85b12
Kijima Daigo <norimaking777@gmail.com>
2025-08-11 21:19:54 +0900

    chore: パフォーマンスモニターを修正+メトリクスを追加した

--- lib/game/game.go
+++ lib/game/game.go
@@ -3,6 +3,7 @@ package game
 import (
 	"fmt"
 	"runtime"
+	"time"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
@@ -44,17 +45,44 @@ func (game *MainGame) Draw(screen *ebiten.Image) {
 	if cfg.Debug {
 		var mem runtime.MemStats
 		runtime.ReadMemStats(&mem)
-		msg := fmt.Sprintf(`FPS: %f
+		
+		// 最後のGCからの経過時間を計算
+		var lastGCTime string
+		if mem.LastGC > 0 {
+			lastGC := time.Unix(0, int64(mem.LastGC))
+			elapsed := time.Since(lastGC)
+			lastGCTime = fmt.Sprintf("%.2fs", elapsed.Seconds())
+		} else {
+			lastGCTime = "N/A"
+		}
+
+		msg := fmt.Sprintf(`FPS: %.1f
 Alloc: %.2fMB
+HeapInuse: %.2fMB
+StackInuse: %.2fMB
+Sys: %.2fMB
+NextGC: %.2fMB
 TotalAlloc: %.2fMB
-Mallocs: %.2fMB
-Frees: %.2fMB
+Mallocs: %d
+Frees: %d
+GC: %d
+LastGC: %s
+PauseTotalNs: %.2fms
+Goroutines: %d
 `,
 			ebiten.ActualFPS(),
-			float64(mem.Alloc/1024/1024),
-			float64(mem.TotalAlloc/1024/1024), // 起動後から割り当てられたヒープオブジェクトの数。freeされてもリセットされない
-			float64(mem.Mallocs/1024/1024),    // 割り当てられているヒープオブジェクトの数。freeされたら減る
-			float64(mem.Frees/1024/1024),      // 解放されたヒープオブジェクトの数
+			float64(mem.Alloc/1024/1024),        // 現在割り当てられているメモリ
+			float64(mem.HeapInuse/1024/1024),    // ヒープで実際に使用中のメモリ
+			float64(mem.StackInuse/1024/1024),   // スタックで使用中のメモリ
+			float64(mem.Sys/1024/1024),          // OSから取得した総メモリ
+			float64(mem.NextGC/1024/1024),       // 次回GC実行予定サイズ
+			float64(mem.TotalAlloc/1024/1024),   // 起動後から割り当てられたヒープオブジェクトの累計バイト数
+			mem.Mallocs,                         // 割り当てられたヒープオブジェクトの回数
+			mem.Frees,                           // 解放されたヒープオブジェクトの回数
+			mem.NumGC,                           // GC実行回数
+			lastGCTime,                          // 最後のGC実行からの経過時間
+			float64(mem.PauseTotalNs)/1000000,   // GC停止時間の累計（ミリ秒）
+			runtime.NumGoroutine(),              // 実行中のGoroutine数
 		)
 		ebitenutil.DebugPrint(screen, msg)
 	}
