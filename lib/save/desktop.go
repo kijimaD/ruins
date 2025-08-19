ecb6607ba5571f68c47349001819c7dcf290032c
Kijima Daigo <norimaking777@gmail.com>
2025-08-19 23:37:37 +0900

    fix: セーブデータメニュー表示にJSON内のタイムスタンプを使う

--- lib/save/desktop.go
+++ lib/save/desktop.go
@@ -6,7 +6,6 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
-	"time"
 )
 
 // initImpl はデスクトップ環境での初期化処理
@@ -45,15 +44,4 @@ func (sm *SerializationManager) saveFileExistsImpl(slotName string) bool {
 	fileName := filepath.Join(sm.saveDirectory, slotName+".json")
 	_, err := os.Stat(fileName)
 	return err == nil
-}
-
-// getSaveFileTimestampImpl はデスクトップ環境でセーブファイルのタイムスタンプを取得する
-func (sm *SerializationManager) getSaveFileTimestampImpl(slotName string) (time.Time, error) {
-	fileName := filepath.Join(sm.saveDirectory, slotName+".json")
-	fileInfo, err := os.Stat(fileName)
-	if err != nil {
-		return time.Time{}, fmt.Errorf("failed to get file info: %w", err)
-	}
-
-	return fileInfo.ModTime(), nil
 }
