c4c3e8d731330870e1b8d7814a94b39b40d9fdd4
Kijima Daigo <norimaking777@gmail.com>
2025-08-27 21:59:12 +0900

    style: lint warning

--- lib/worldhelper/sort_test.go
+++ lib/worldhelper/sort_test.go
@@ -104,6 +104,7 @@ func TestSortEntities(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
 			entities := tt.entities()
 			sorted := SortEntities(world, entities)
 
