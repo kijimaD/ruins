f6aed2e162937f4cdb6287dfc82b109297aa7cc2
Kijima Daigo <norimaking777@gmail.com>
2025-09-28 23:04:31 +0900

    chore: 実際の設定ファイルと形式を合わせる

--- lib/mapspawner/test_helper.go
+++ lib/mapspawner/test_helper.go
@@ -10,26 +10,17 @@ func createMapspawnerTestRawMaster() *raw.Master {
 [[tile]]
 Name = "Floor"
 Description = "床タイル"
-Type = "FLOOR"
 Walkable = true
-BlocksLOS = false
-SpriteNumber = 1
 
 [[tile]]
 Name = "Wall"
 Description = "壁タイル"
-Type = "WALL"
 Walkable = false
-BlocksLOS = true
-SpriteNumber = 2
 
 [[tile]]
 Name = "Empty"
 Description = "空のタイル"
-Type = "EMPTY"
 Walkable = false
-BlocksLOS = false
-SpriteNumber = 0
 `)
 	return &rawMaster
 }
