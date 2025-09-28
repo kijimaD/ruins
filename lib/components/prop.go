a6c1ac270e08c4d91edc079005af2cf66fba6e54
Kijima Daigo <norimaking777@gmail.com>
2025-09-28 12:22:42 +0900

    refactor: 未対応のpropを削除した

--- lib/components/prop.go
+++ lib/components/prop.go
@@ -3,11 +3,6 @@ package components
 // PropType は置物のタイプを表す
 type PropType string
 
-// String はPropTypeの文字列表現を返す
-func (pt PropType) String() string {
-	return "PropType"
-}
-
 const (
 	// PropTypeTable はテーブル
 	PropTypeTable PropType = "table"
@@ -17,14 +12,6 @@ const (
 	PropTypeBookshelf PropType = "bookshelf"
 	// PropTypeBed はベッド
 	PropTypeBed PropType = "bed"
-	// PropTypeChest は宝箱
-	PropTypeChest PropType = "chest"
-	// PropTypeDoor はドア
-	PropTypeDoor PropType = "door"
-	// PropTypeTorch はたいまつ
-	PropTypeTorch PropType = "torch"
-	// PropTypeAltar は祭壇
-	PropTypeAltar PropType = "altar"
 	// PropTypeBarrel は樽
 	PropTypeBarrel PropType = "barrel"
 	// PropTypeCrate は木箱
